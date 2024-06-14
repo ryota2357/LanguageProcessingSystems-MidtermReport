31c9a5c6baa928e9740f669fa70d46149b35b23d
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-06-14 09:33:19 +0900

    add mode selection

--- src/main.rs
+++ src/main.rs
@@ -30,24 +30,47 @@ enum Stmt {
 }
 
 fn main() {
-    fn question(msg: &str) -> String {
-        let mut stdout = stdout();
-        stdout.write_all(msg.as_bytes()).unwrap();
-        stdout.flush().unwrap();
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-        input
-    }
+    let mode = loop {
+        let res = question("Mode (1: interpreter, 2: vm): ")
+            .trim()
+            .parse::<u32>();
+        match res {
+            Ok(1) => break 1,
+            Ok(2) => break 2,
+            _ => continue,
+        }
+    };
 
-    let mut map = HashMap::new();
+    let mut env = HashMap::new();
+    if mode == 1 {
+        interpreter_mode(&mut env);
+    } else {
+        vm_mode(&mut env);
+    }
+}
+
+fn question(msg: &str) -> String {
+    let mut stdout = stdout();
+    stdout.write_all(msg.as_bytes()).unwrap();
+    stdout.flush().unwrap();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    input
+}
+
+fn interpreter_mode(env: &mut HashMap<String, f64>) {
     loop {
         let line = question("> ");
         let tokens = lexer::parse(&line);
         let stmt = parser::parse(&tokens);
-        let res = interpreter::eval(&stmt, &mut map);
+        let res = interpreter::eval(&stmt, env);
         match res {
             (Some(name), res) => println!("{} = {}", name, res),
             (None, res) => println!("= {}", res),
         }
     }
+}
+
+fn vm_mode(_env: &mut HashMap<String, f64>) {
+    unimplemented!();
 }
